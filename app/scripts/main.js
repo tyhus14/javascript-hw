09c72444364bcf502794569095c871659910b433
tyhus14 <huston.tyler3@gmail.com>
2014-01-22 22:20:05 -0500

    added more javascript examples

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,13 +5,11 @@ $('.box').hover(function(){
 	console.log('the button clicked');
 
 	$(this).addClass('active');
-
-	$('.box-three').addClass('visible');
 });
 
 
 $( "h1" ).click(function() {
-  $( this ).slideUp();
+  $(this).slideUp();
 });
 
 
@@ -40,6 +38,21 @@ $('.box-six').hover(function(){
 	$(this).addClass('larger');
 });
 
+
+$('.box-seven').click(function(){
+	console.log('the button clicked');
+
+	$('.minibox').addClass('appear');
+});
+
+$('.box-eight').click(function(){
+	console.log('the button clicked');
+
+	$(this).addClass('largerspin');
+});
+
+
+
 $('.ball').click(function(){
 	console.log('the button clicked');
 
@@ -48,7 +61,6 @@ $('.ball').click(function(){
 	$('.bar').addClass('spin');
 
 	$('.ball-two').addClass('fly');
-
 });
 
 $('.stick').click(function(){
@@ -58,10 +70,82 @@ $('.stick').click(function(){
 
 	$('.cueball').addClass('active');
 
-	$('.eightball').addClass('next');
+	$('.eightball').addClass('next', 'away');
 
-	$('.eightball').addClass('gone');
+	$('.pooltable span').addClass('change');
 
+});
 
+$('.write').focus(function(){
+	$(this).css('background-color', 'red');
 
+});
+
+$('.write').blur(function(){
+	$(this).css('background-color', 'white');
+});
+
+$('.rectangle').click(function(){
+	$('.toggle').trigger('focus');
+});
+
+$('.box-ten').click(function(){
+	$(this).append("<span> More Text Added! </span>");
+});
+
+$('.box-eleven').mouseleave(function(){
+	console.log('mouse left');
+	$(this).removeClass('gone');
+});
+
+$('.box-twelve').click(function(){
+	console.log('the button clicked');
+	$(this).addClass('active');
+});
+
+$('.buttonlever').hover(function(){
+	console.log('the button hovered');
+	$('.box-thirteen').addClass('active');
+});
+
+$('.box-fourteen').click(function(){
+	console.log('the button clicked');
+	$(this).toggleClass('active');
+});
+
+$('.box-fifteen').hover(function(){
+	console.log('the button clicked');
+	$(this).toggleClass('active');
+});
+
+$('.box-sixteen').click(function(){
+	console.log('the button clicked');
+	$(this).toggleClass('active');
+});
+
+$('.box-seventeen').hover(function(){
+	console.log('the button hovered');
+	$(this).toggleClass('active');
+});
+
+$('.box-eighteen').click(function(){
+	console.log('the button clicked');
+	$(this).toggleClass('active');
+});
+
+$('.box-nineteen').click(function(){
+	console.log('the button clicked');
+	
+	$(this).addClass('active');
+	$('.littlebox').addClass('active');
+});
+
+$('.box-twentyone').click(function(){
+	console.log('the button clicked');
+	
+	$(this).addClass('active');
+});
+
+$('.box-twenty').click(function() {
+  $(this).slideUp();
 });
